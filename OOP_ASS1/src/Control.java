1f29a089ed33148ad1d995ce3add94428a899af9
andrixxz <152204906+andrixxz@users.noreply.github.com>
2024-04-19 23:07:32 +0100

    Update Control.java

--- OOP_ASS1/src/Control.java
+++ OOP_ASS1/src/Control.java
@@ -5,22 +5,31 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 public class Control {
+	// instance of the Gui class
     private Gui gui;
 
     public Control() {
+    	// intialises the GUI
         gui = new Gui();
+        // adds the ActionListener to the file button
         gui.getFileButton().addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+            	// selects the file
                 File[] files = FileSelector.selectFiles();
+                // if files are selected
                 if (files != null) {
+                	// gets the search term from the input field
                     String searchWord = gui.getInputField().getText();
+                    // iterates through each selected file
                     for (File file : files) {
+                    	// searches for the search word in the file and updates GUI
                         FileProcessor.searchWordInFile(file, searchWord, gui);
                     }
-                    // After searching in all files, print files in highest ranking order
+                    // after searching in all files, prints files in highest ranking order
                     FileProcessor.printFilesInHighestRankingOrder(gui);
                     
                 } else {
+                	// if no files are selected
                     gui.appendToConsole("No File Selected Or Opened");
                 }
             }
@@ -28,6 +37,8 @@ public class Control {
     }
 
     public static void main(String[] args) {
+    	// creates an instance of control to start the program 
         new Control();
+        
     }
 }

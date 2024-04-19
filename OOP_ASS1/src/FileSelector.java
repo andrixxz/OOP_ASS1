42a98ded4cd5862587367bb69d2f532695f051a5
andrixxz <152204906+andrixxz@users.noreply.github.com>
2024-04-19 23:33:14 +0100

    Update FileSelector.java

--- OOP_ASS1/src/FileSelector.java
+++ OOP_ASS1/src/FileSelector.java
@@ -7,10 +7,12 @@ public class FileSelector {
     public static File[] selectFiles() {
         int result;
         JFileChooser fileSelector = new JFileChooser();
+        // allows multiple files to be chosen
         fileSelector.setMultiSelectionEnabled(true);
 
         result = fileSelector.showOpenDialog(fileSelector);
 
+        // checks if the user has selected and opened a file
         if (result == JFileChooser.APPROVE_OPTION) {
             return fileSelector.getSelectedFiles();
         } else {
